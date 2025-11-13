2b35213bea112469923c33ae93a7b62bd6d31772
sibeschu <ben.schuhknecht@gmx.net>
2025-11-13 10:48:19 +0100

    cleanup

--- src/igus_student/igus_student/simple_robot_control.py
+++ src/igus_student/igus_student/simple_robot_control.py
@@ -44,8 +44,8 @@ COLLISION_OBJECTS = [
     {
         'name': 'table',
         'shape': 'box',
-        'size': [1.2, 0.8, 0.02],
-        'position': (0.5, 0.0, -0.01),
+        'size': [2.5, 0.8, 0.02],
+        'position': (0.0, 0.0, -0.01),
         'orientation': (0.0, 0.0, 0.0, 1.0)
     }
 ]
